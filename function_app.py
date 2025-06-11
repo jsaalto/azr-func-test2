08fc80031cda377f5f184b7940d4147bcb9fda25
Jeff Aalto <jeff.aalto@centricconsulting.com>
2025-06-11 08:33:18 -0400

    Add Metadata pull

--- function_app.py
+++ function_app.py
@@ -1,3 +1,7 @@
+import requests
+from PIL import Image
+from io import BytesIO
+
 import azure.functions as func
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
@@ -7,8 +11,15 @@ def get_image_exif_metadata(req: func.HttpRequest) -> func.HttpResponse:
     
     image_url_variable = req.params.get('image_url_param')
     if image_url_variable:
+        try:
+            response = requests.get(image_url_variable)
+            response.raise_for_status()
+            image = Image.open(BytesIO(response.content))
+            exif_data = image.getexif()
+        except Exception as e:
+            exif_data = None
         return func.HttpResponse(
-            f"This HTTP triggered function executed successfully. The image URL is: {image_url_variable}",
+            f"This HTTP triggered function executed successfully. The image URL is {image_url_variable}, and the EXIF metadata is: {exif_data}",
             status_code=200
         )  
     else:
